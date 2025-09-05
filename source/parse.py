31057ca5c56bbba6bdbbc8d9fbbd8b6b0ec00cf7
Rixu <ricorx9@gmail.com>
2025-09-06 01:06:39 +0300

    add html parser

---
+++ source/parse.py
@@ -0,0 +1,15 @@
+def cut(html: str, block: str) -> str:
+    key: str = "[" + block
+    i: int = html.find(key)
+    if i == -1: return html
+    return html[:i] + html[i + len(key):].replace("]", "", count=1)
+
+# wash off unselected
+def wash(html: str) -> str:
+    b: int = html.find("[")
+    while b != -1:
+        e: int = html[b:].find("]") + 1
+        if e == 0: break
+        html = html[:b] + html[(b + e):]
+        b = html.find("[")
+    return html
