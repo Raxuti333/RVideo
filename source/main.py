e5593aaf2a8b9240226f391abfcf7b2540cb56ad
Rixu <ricorx9@gmail.com>
2025-09-05 17:17:07 +0300

    add favicon

--- source/main.py
+++ source/main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def root():
     return "Hello World"
 
-app.run("0.0.0.0", 3000)
+@app.route("/favicon.ico")
+def favicon():
+    return send_file(app.root_path + "/favicon.ico", mimetype='image/vnd.microsoft.icon')
+
+app.run("0.0.0.0", 8080)
