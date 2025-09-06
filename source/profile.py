ec13b427ae275518e3033278f7ba35623e1fa60d
Rixu <ricorx9@gmail.com>
2025-09-07 02:41:21 +0300

    changed profile logged out behaviour

--- source/profile.py
+++ source/profile.py
@@ -1,20 +1,18 @@
-from flask import Flask, session, send_file
+from flask import Flask, session, send_file, redirect
 from parse import cut, wash
 
-def page(app: Flask):
+def profile_page(app: Flask):
     # Replace with non-garbage collection reliant method
     html: str = open(app.root_path + "/html/profile.html").read()
 
     if session.get("profile") == None:
-        return wash(cut(html, "NO_PROFILE"))
-    
-    html = cut(html, "PROFILE")
+        return redirect("/")
 
     html = wash(html)
     return html
 
 # Support profile pictures
-def pfp(app: Flask):
+def profile_picture(app: Flask):
     if session.get("profile") == None:
-        return send_file(app.root_path + "/no-pfp.png", mimetype='image/png')
-    return send_file(app.root_path + "/no-pfp.png", mimetype='image/png')
+        return send_file(app.root_path + "/images/no-pfp.png", mimetype='image/png')
+    return send_file(app.root_path + "/images/no-pfp.png", mimetype='image/png')
