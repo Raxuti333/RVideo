d420606fd4e7e0e8b67d41e843bc9bb7a298a05d
Rixu <ricorx9@gmail.com>
2025-09-07 01:27:45 +0300

    added invalid form redirect

--- source/signup.py
+++ source/signup.py
@@ -1,13 +1,16 @@
-from flask import Flask, session, redirect, url_for, request, flash, get_flashed_messages
+from flask import Flask, session, redirect, request, flash, get_flashed_messages
 from parse import cut, wash, validate
 from werkzeug.security import generate_password_hash
 import db
 
-def no_match(html, unused) -> str:
+def no_match(html: str, unused) -> str:
     return cut(html, "NO_MATCH")
 
-def exists(html: str, username: str):
+def exists(html: str, username: str) -> str:
     return cut(html.replace("$USERNAME", username), "EXISTS")
+
+def invalid(html: str, unused) -> str:
+    return cut(html, "INVALID")
 
 def signup_page(app: Flask):
     html: str = open(app.root_path + "/html/signup.html").read()
@@ -18,29 +21,35 @@ def signup_page(app: Flask):
 
     list = get_flashed_messages()
     if list != []:
-        JTABLE: dict[str, int] = { "NO_MATCH": no_match, "EXISTS": exists }
-        html = JTABLE[list[0]](html, list[-1])
+        try:
+            JTABLE: dict[str, int] = { "NO_MATCH": no_match, "EXISTS": exists, "INVALID": invalid }
+            html = JTABLE[list[0]](html, list[-1])
+        except:
+            pass
 
     html = wash(html)
     return html
 
-# this is run in try expect block so failing is fine
 def signup_create(app: Flask):
-    username = validate(request.form["username"])
-    password = request.form["password"]
-    c_pass   = request.form["confirm_password"]
+    try:
+        username = validate(request.form["username"])
+        password = request.form["password"]
+        c_pass   = request.form["confirm_password"]
+    except:
+        flash("INVALID")
+        return redirect("/signup")
 
     if password != c_pass:
         flash("NO_MATCH")
-        return redirect(url_for("signup"), 303)
+        return redirect("/signup", 303)
     
     # Check if username is taken
     if db.query("SELECT COUNT(username) FROM profile WHERE username = ?", (username,))[0] != 0:
         flash("EXISTS")
         flash(username)
-        return redirect(url_for("signup"), 303)
+        return redirect("/signup", 303)
 
     # Create new profile
     db.query("INSERT INTO profile (username, password) VALUES(?, ?)", (username, generate_password_hash(password)))
 
-    return redirect(url_for("profile"), 303)
+    return redirect("/profile", 303)
