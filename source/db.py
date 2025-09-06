685e038774a5b4040553bf8373cac15d514991c3
Rixu <ricorx9@gmail.com>
2025-09-07 01:28:23 +0300

    added db fetch count

--- source/db.py
+++ source/db.py
@@ -1,11 +1,10 @@
 import sqlite3
-from parse import config
 
-def query(sql: str, params = []):
+def query(sql: str, params = [], count = 1):
     db = sqlite3.connect("db")
     db.row_factory = sqlite3.Row
 
-    v = db.execute(sql, params)
+    v = db.execute(sql, params).fetchmany(count)
     db.commit()
 
     db.close()
