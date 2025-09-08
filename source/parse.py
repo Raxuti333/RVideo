283358a8fcf11d8d4ae283265d671b8c0f6bbf44
Rixu <ricorx9@gmail.com>
2025-09-09 01:17:36 +0300

    beautified parser with pylint suggestions

--- source/parse.py
+++ source/parse.py
@@ -1,20 +1,32 @@
-import db, re
+import re, db
 
-VIDEO_HTML: str = """ 
+VIDEO_HTML: str = """
 <a href=\"video?view=$VID\" class="video">
     <img style=\"display: inline; width: 48px; heigth: 48px\" src=\"profile?pfp=$PID\">
     <p style=\"display: inline; position: relative; bottom: 15px;\">$TITLE</p>
 </a>
 """
 
+def replace(html: str, params: list[tuple]) -> str:
+    """ replaces all listed parameters """
+    for p in params:
+        html = html.replace(p[0], p[1])
+    return html
+
+def read(local_path: str) -> str:
+    """ read utf-8 encoded text file """
+    with open(local_path, encoding="utf-8") as f:
+        return f.read()
+
 def cut(html: str, block: str) -> str:
+    """ cut field """
     key: str = "[" + block
     i: int = html.find(key)
     if i == -1: return html
     return html[:i] + html[i + len(key):].replace("]", "", count=1)
 
-# wash off unselected
 def wash(html: str) -> str:
+    """ remove uncut fields """
     b: int = html.find("[")
     while b != -1:
         e: int = html[b:].find("]") + 1
@@ -24,30 +36,32 @@ def wash(html: str) -> str:
     return html
 
 def config(field: str) -> str:
-    # Replace with non-garbage collection reliant method
-    return wash(cut(open(".config").read(), field)).replace("\n", "").replace(" ", "")
+    """ read field from .config """
+    return wash(cut(read(".config"), field)).replace("\n", "").replace(" ", "")
 
-def validate(input: str) -> str:
-    return input.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("[", "&#91;").replace("]", "&#93;").replace("$", "&#36;")
+def validate(user_str: str) -> str:
+    """ validate user input """
+    return user_str.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("[", "&#91;").replace("]", "&#93;").replace("$", "&#36;")
 
 def search(req: str):
+    """ search for video in db """
     html: str = ""
 
     limit = 20
     params = []
     sql: str = "SELECT * FROM video WHERE"
 
-    # Generate sql query with parameters 
+    # Generate sql query with parameters
     for m in req.split(","):
         p = m.split("=")
         match(p[0]):
             case "BEFORE":
-                if re.match(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$", p[-1]) != None or p[-1] == "now":
+                if re.match(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$", p[-1]) is not None or p[-1] == "now":
                     sql += f" (strftime(\"%s\", date) - strftime(\"%s\", \"{p[-1]}\")) < 0"
                 else: sql += " 1"
                 sql += " AND"
             case "AFTER":
-                if re.match(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$", p[-1]) != None or p[-1] == "now":
+                if re.match(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$", p[-1]) is not None or p[-1] == "now":
                     sql += f" (strftime(\"%s\", date) - strftime(\"%s\", \"{p[-1]}\")) > 0"
                 else: sql += " 1"
                 sql += " AND"
@@ -77,6 +91,11 @@ def search(req: str):
 
     videos = db.query(sql, params, limit)
     for v in videos:
-        html += VIDEO_HTML.replace("$VID", str(v["vid"])).replace("$PID", str(v["pid"])).replace("$TITLE", v["name"])
+        html += replace(
+            VIDEO_HTML,
+            [("$VID", str(v["vid"])),
+             ("$PID", str(v["pid"])),
+             ("$TITLE", v["name"])]
+            )
 
     return html
