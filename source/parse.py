a62b85db8f3405b25033ba217f93217d34fd2b8d
Rixu <ricorx9@gmail.com>
2025-09-08 03:43:31 +0300

    added searching for videos and advanced search

--- source/parse.py
+++ source/parse.py
@@ -1,3 +1,12 @@
+import db, re
+
+VIDEO_HTML: str = """ 
+<a href=\"video?view=$VID\" style=\"text-decoration: none; display: inline;\">
+    <img style=\"display: inline; width: 48px; heigth: 48px\" src=\"profile?pfp=$PID\">
+    <p style=\"display: inline; position: relative; bottom: 15px;\">$TITLE</p>
+</a>
+"""
+
 def cut(html: str, block: str) -> str:
     key: str = "[" + block
     i: int = html.find(key)
@@ -20,3 +29,50 @@ def config(field: str) -> str:
 
 def validate(input: str) -> str:
     return input.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("[", "&#91;").replace("]", "&#93;").replace("$", "&#36;")
+
+def search(req: str):
+    html: str = ""
+
+    limit = 20
+    params = []
+    sql: str = "SELECT * FROM video"
+
+    # Generate sql query with parameters 
+    for m in req.split(","):
+        p = m.split("=")
+        match(p[0]):
+            case "BEFORE":
+                if re.match(r"^\d{2}-\d{2}-\d{4}$", p[-1]) or p[-1] == "now":
+                    sql += f" WHERE timediff(date, '{p[-1]}') > 0"
+            case "AFTER":
+                if re.match(r"^\d{2}-\d{2}-\d{4}$", p[-1]) or p[-1] == "now":
+                    sql += f" WHERE timediff(date, '{p[-1]}') < 0"
+            case "USER":
+                user = db.query("SELECT pid FROM profile WHERE LOWER(username) = LOWER(?)", [p[-1]])
+                if user != []:
+                    sql += f" WHERE pid = {user[0]["pid"]}"
+                else:
+                    sql += " WHERE pid = 0"
+            case "USERS":
+                users = db.query("SELECT pid FROM profile WHERE username LIKE ?", ["%" + p[-1] + "%"], 5)
+                if users != []:
+                    sql += " WHERE"
+                    for u in users:
+                        sql += f" pid = {u["pid"]} OR"
+                    sql = sql[:len(sql) - 2]
+            case "LIMIT":
+                if p[-1].isdigit(): 
+                    if int(p[-1]) < 100:
+                        limit = int(p[-1])
+            case _:
+                sql += " WHERE"
+                for x in m.split(" "):
+                    params.append("%"+x+"%")
+                    sql += " name LIKE ? OR"
+                sql = sql[:len(sql) - 2]
+
+    videos = db.query(sql, params, limit)
+    for v in videos:
+        html += VIDEO_HTML.replace("$VID", str(v["vid"])).replace("$PID", str(v["pid"])).replace("$TITLE", v["name"])
+
+    return html
--- source/root.py
+++ source/root.py
@@ -1,9 +1,5 @@
 from flask import Flask, session, request, send_file, redirect, flash, get_flashed_messages
-from parse import cut, wash
-import db
-
-def root_favicon(app: Flask):
-    return send_file(app.root_path + "/images/favicon.ico", mimetype='image/vnd.microsoft.icon')
+from parse import cut, wash, search
 
 def root_page(app: Flask):
     html: str = open(app.root_path + "/html/root.html").read()
@@ -15,10 +11,13 @@ def root_page(app: Flask):
 
     embed: str = ""
 
-    search = request.form.get("search")
-    if search == None:
-        videos = db.query("SELECT * FROM video ORDER BY datetime(date) DESC LIMIT 20", count=20)
-        for v in videos:
-            embed += "<a href=\"video?view=" + str(v["vid"]) + "\"><div style=\"display: inline-block; margin: 15px\">" + "<img src=\"profile?pfp=" + str(v["pid"]) + "\" style=\"display: inline-block; width: 48px; height: 48px;\"><p style=\"display: inline-block;\">" + v["name"] + "<p></div></a>"
+    req = request.form.get("search")
+    if req == None:
+        embed = search("BEFORE=now")
+    else:
+        embed = search(req)
 
     return wash(html.replace("$VIDEOS", embed))
+
+def root_favicon(app: Flask):
+    return send_file(app.root_path + "/images/favicon.ico", mimetype='image/vnd.microsoft.icon')
