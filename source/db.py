d71691a60deb61be216978d836224547c04bf3d5
Rixu <ricorx9@gmail.com>
2025-09-09 12:59:33 +0300

    updated db query function

---
+++ source/db.py
@@ -0,0 +1,26 @@
+""" TODO """
+
+import sqlite3
+
+def query(sql: str, params: list = [], count: int = 1) -> sqlite3.Row | sqlite3.Cursor:
+    """ query data from sqlite3 db
+    count = 1: fetchone()
+    count = -1: fetchall()
+    count = n: fetchmany(n)
+    count = 0: None
+    """
+    db = sqlite3.connect("db")
+    db.row_factory = sqlite3.Row
+
+    cursor = db.execute(sql, params)
+    db.commit()
+
+    match(count):
+        case 1: result = cursor.fetchone()
+        case -1: result = cursor.fetchall()
+        case 0: result = None
+        case _: result = cursor.fetchmany(count)
+
+    db.close()
+
+    return result
