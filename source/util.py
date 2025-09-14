5c5e67ded10587222034faa5a0a95039431a0caa
Rixu <ricorx9@gmail.com>
2025-09-14 03:20:53 +0300

    added and fixed util functions

--- source/util.py
+++ source/util.py
@@ -1,7 +1,19 @@
 """ TODO """
 
 from secrets import token_hex
-from flask import session
+from flask import session, request, flash, get_flashed_messages
+
+def get_flash() -> list[str]:
+    """ get messages from flash """
+    flashed = get_flashed_messages()
+    if flashed == []:
+        return [""]
+    return flashed
+
+def set_flash(messages: list[str]) -> None:
+    """ set messages to flash """
+    for msg in messages:
+        flash(msg)
 
 def get_session_token() -> str:
     """ return existing session token or create a session token """
@@ -10,10 +22,44 @@ def get_session_token() -> str:
     return session["token"]
 
 def get_account() -> dict | None:
-    """ returns account or none isn't found """
+    """ returns account or none if it isn't found """
     if session.get("account") is None:
         return None
     return session["account"]
+
+def set_account(key: str, value) -> None:
+    """ sets value to account if it's found """
+    if session.get("account") is None:
+        return
+    session["account"][key] = value
+
+def get_form(fields: list[(str, type)]) -> dict:
+    """ returns dictionary containing fields values """
+    form: dict = {}
+    for f, t in fields:
+        if t is bool:
+            form[f] = request.form.get(f, type=str) == "True"
+        else:
+            form[f] = request.form.get(f, type=t)
+    return form
+
+def get_query() -> list[str]:
+    """ 
+        returns query strings list 
+        TODO support complex queries
+    """
+    binary: bytes = request.query_string
+    if not binary:
+        return [""]
+    return str(binary, encoding="utf-8").split("=")
+
+def check_password(password: str) -> tuple[bool, str]:
+    """ checks if password is acceptable """
+    if len(password) < 4:
+        return (False, "password too short")
+    if not any(ch in password for ch in r"1234567890 _\?!@$%{}[]\+-/\\"):
+        return (False, "password must contain at leas one special character")
+    return (True, "Success")
 
 def config(field: str) -> str:
     """ returns fields value from .config """
