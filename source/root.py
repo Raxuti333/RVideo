3619bf4cbf72c6fe6c37f2d1eb7318d2bd53d486
Rixu <ricorx9@gmail.com>
2025-09-14 03:50:25 +0300

    fixed root crash

--- source/root.py
+++ source/root.py
@@ -1,7 +1,19 @@
-""" TODO """
+""" Landing page processing i.e video search functions """
 
 from flask import render_template
+from util import get_session_token, get_account, get_form
 
 def root_page() -> str:
     """ service function for landing page """
-    return render_template("root.html")
+
+    token   = get_session_token()
+    account = get_account()
+    form    = get_form([
+                         ("search", str),
+                         ("select", str),
+                         ("date",   str),
+                         ("user",   str),
+                         ("token",  str)
+                        ])
+    
+    return render_template("root.html", token=token, account=account)
