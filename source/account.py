5c7000a429368ba08b1d06ecbef028f5ef126592
Rixu <ricorx9@gmail.com>
2025-09-14 20:12:59 +0300

    proper profile picture loading

--- source/account.py
+++ source/account.py
@@ -1,13 +1,38 @@
 """ TODO """
 
-def account_page() -> str:
+from flask import render_template, redirect
+from util import get_query, get_method, get_filename, send_data
+import db
+
+def account_page():
     """ account page """
-    return ""
 
-def account_picture() -> str:
+    query = get_query()
+
+    if get_method() == "POST":
+        return account_edit()
+
+    if query[0] == "picture":
+        return account_picture(query)
+
+    return render_template("account.html")
+
+def account_picture(query: list[str]):
     """ serve account picture  """
-    return ""
 
-def account_create() -> str:
-    """ edit and create account """
+    mimetype: dict[str, str] = {
+        "png": "image/png", 
+        "jpg": "image/jpeg",
+        "ico": "image/vnd.microsoft.icon",
+        "bmp": "image/bmp"
+        }
+
+    path = get_filename(query[-1], "pfp", ["png", "jpg", "ico", "bmp"])
+    if path is None:
+        return redirect("/static/no-pfp.png")
+
+    return send_data(path, mimetype[path.split(".")[-1]])
+
+def account_edit():
+    """ edit account """
     return ""
