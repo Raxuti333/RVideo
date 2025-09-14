3f9df860b184b4987f3065f61da200dd50f3711a
Rixu <ricorx9@gmail.com>
2025-09-14 03:44:08 +0300

    added .config and run.sh

---
+++ env/run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+venv=../.venv/bin/activate
+main=../source/main.py
+
+# set cwd to this direcotry containing this file.
+cd "$(dirname "$0")"
+
+if [ ! -f db ]; then
+    echo "creating new database to $PWD/db"
+    sqlite3 db <<EOF
+CREATE TABLE profile ( pid INTEGER PRIMARY KEY, username TEXT, password TEXT );
+CREATE TABLE video ( vid INTEGER PRIMARY KEY, pid INTEGER, name TEXT, description TEXT, date TEXT, FOREIGN KEY(pid) REFERENCES profile(pid) );
+CREATE TABLE comment ( vid INTEGER, pid INTEGER, text TEXT, date TEXT, FOREIGN KEY(vid) REFERENCES video(vid), FOREIGN KEY(pid) REFERENCES profile(pid) );
+EOF
+fi
+
+if [ ! -f $venv ]; then
+    echo "No venv found"
+    exit 1
+fi
+
+if [ ! -f $main ]; then
+    echo "No main.py found"
+    exit 1
+fi
+
+mkdir -p pfp
+mkdir -p video
+
+# if moving env update these to point to the right files
+source $venv
+python3 $main
