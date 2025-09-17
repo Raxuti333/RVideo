9fe1fb072f8395daf6ffc7a12aaa083e20deef7c
Rixu <ricorx9@gmail.com>
2025-09-17 03:46:58 +0300

    added commenting

--- source/comment.py
+++ source/comment.py
@@ -1,5 +1,35 @@
-""" TODO """
+""" process comment forms """
+
+from flask import redirect
+from util import get_form, get_account, get_session_token
+import db
 
 def comment_form():
-    """ TODO """
-    return ""
+    """ process comment form """
+
+    account = get_account()
+    token   = get_session_token()
+    form    = get_form([
+    ("vid", int),
+    ("token", str),
+    ("comment", str),
+    ])
+
+    if account is None:
+        return redirect("/login")
+
+    if form["vid"] is None or form["comment"] is None:
+        return redirect("/")
+
+    link: str = "/video?view=" + str(form["vid"])
+
+    if token != form["token"]:
+        return redirect(link)
+
+    db.query(
+    "INSERT INTO comment (vid, pid, text, date) VALUES(?, ?, ?, unixepoch('now'))",
+    [form["vid"], account["pid"], form["comment"]],
+    0
+    )
+
+    return redirect(link)
--- source/video.py
+++ source/video.py
@@ -39,12 +39,14 @@ def video_view(account: dict, token: str, query: list[str]):
         return redirect("/")
 
     target = db.query("SELECT pid, username FROM profile WHERE pid = ?", [video["pid"]])
+    comments = db.query("SELECT pid, text FROM comment WHERE vid = ?", [vid], -1)
 
     return render_template("view.html",
                            token = token,
                            account = account,
                            video = video,
                            target = target,
+                           comments = comments,
                            vid = vid
                           )
 
