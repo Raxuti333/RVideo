937241d8e69e84e7cf5307e00eddfd47ebecbf7f
Rixu <ricorx9@gmail.com>
2025-10-14 01:25:17 +0300

    new .config index.sql

--- env/index.sql
+++ env/index.sql
@@ -1,4 +1,4 @@
 CREATE INDEX IF NOT EXISTS idx_vid_timestamp ON comment(vid, timestamp);
 CREATE INDEX IF NOT EXISTS idx_vid_private ON video(vid, private);
 CREATE INDEX IF NOT EXISTS idx_pid_private ON video(pid, private);
-CREATE INDEX IF NOT EXISTS  idx_text ON tag(text);
+CREATE INDEX IF NOT EXISTS idx_text ON tag(text);
